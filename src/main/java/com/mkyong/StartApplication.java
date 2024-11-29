6995923a23fe8c8e8050a156a4a3983623c9ed72
mynewcreations <mynewcreations@gmail.com>
2024-11-29 12:43:27 +0530

    Update StartApplication.java

--- src/main/java/com/mkyong/StartApplication.java
+++ src/main/java/com/mkyong/StartApplication.java
@@ -13,7 +13,7 @@ public class StartApplication {
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "Docker + Spring Boot");
-        model.addAttribute("msg", "Welcome to the docker container NAVEEN !");
+        model.addAttribute("msg", "Welcome to the docker container NAVEEN build 23 !");
         return "index";
     }
 
